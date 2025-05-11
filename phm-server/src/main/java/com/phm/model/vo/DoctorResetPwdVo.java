31604c60415e5e2f9674edadb84c60570ec7dd9c
YangHgRi <yanghgri@gmail.com>
2025-05-11 18:42:24 +0800

    1. 进一步消除邮箱验证模块的存在痕迹

--- phm-server/src/main/java/com/phm/model/vo/DoctorResetPwdVo.java
+++ phm-server/src/main/java/com/phm/model/vo/DoctorResetPwdVo.java
@@ -3,8 +3,7 @@ package com.phm.model.vo;
 /**
  * 医生重置密码
  *
- * @param password  新密码
- * @param checkCode 验证码
+ * @param password 新密码
  */
-public record DoctorResetPwdVo(String password, String checkCode) {
+public record DoctorResetPwdVo(String password) {
 }
